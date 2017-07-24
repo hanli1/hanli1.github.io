426c84899ca71c2ba20277182ce91559189fb461
Han Li <hl565@cornell.edu>
2017-07-23 23:48:58 -0700

    changed to create react app

--- src/IntroductionView.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class IntroductionView extends React.Component {
-  render() {
-    return (
-      <h1>Hi, my name is {this.props.name}</h1>
-    );
-  }
-}
--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import IntroductionView from './IntroductionView.js';
-
-class App extends React.Component {
-  render () {
+class App extends Component {
+  render() {
     return (
-      <div>
-        <p> Hello React!</p>
-        <IntroductionView name="Han"/>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
-      
-    )
+    );
   }
 }
 
-render(<App/>, document.getElementById('app'));
+export default App;
