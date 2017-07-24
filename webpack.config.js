b2bed425b711522377eceea7c29e1374786c0520
Han Li <hl565@cornell.edu>
2017-07-23 21:37:03 -0700

    set up webpack and hello world

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'build/');
+var APP_DIR = path.resolve(__dirname, 'src/');
+
+var config = {
+  entry: {
+    javascript: APP_DIR + '\\app.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        loader: "file-loader?name=[name].[ext]",
+      },
+    ],
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  }
+};
+
+module.exports = config;
